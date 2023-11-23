73c474d59805007960e21920e26553c913dc6bf8
Songji <qkrthdwl123@gmail.com>
2023-11-23 09:06:34 +0900

    FastAPI

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ async def upload_file(file: UploadFile):
     if ( not file ):
         return {"message": "파일이 없습니다."}
     else:
-        save_dir = os.path.join("C:\Hong\KDT\FastAPI", file.filename)
+        save_dir = os.path.join("C:\songji\KDT\FastAPI", file.filename)
         with open(save_dir, "wb") as dest_file:
             sh.copyfileobj(file.file, dest_file)
         return {"message": "success"}
